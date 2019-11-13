d0f8d0ec2839394d9b8ac95bf25696715ad81330
Matheus Carmo <matheus_fellip@hotmail.com>
2019-11-12 21:07:06 -0300

    dev

---
+++ app/Http/Controllers/PessoasController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PessoasController extends Controller
+{
+    public function index()
+    {
+        return false;
+    }
+}
